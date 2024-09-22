b6ee459986ec364a3d52735179965526ae461ff1
victormoraes <victor.moraesrj@gmail.com>
2024-09-22 14:18:56 -0300

    feat: posts crud

--- backend/index.js
+++ backend/index.js
@@ -27,10 +27,44 @@ app.post("/posts", async (req, res) => {
         await newPost.save();
         res.status(201).json({success: true, data: newPost });
     } catch (error) {
-        console.log("Error in creating post: ",error); // Remover em prod
         res.status(500).json({success: false, message: "Failed to create post" });
     }
 })
+
+app.delete("/posts/:id", async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        await Post.findByIdAndDelete(id);
+        res.status(204).json({success: true});
+    } catch (error) {
+        res.status(500).json({success: false, message: error });
+    }
+})
+
+app.get("/posts", async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json({success: true, data: posts });
+    } catch (error) {
+        res.status(500).json({success: false, message: error });
+    }
+})
+
+app.put("/posts/:id", async (req, res) => {
+    const {id} = req.params;
+    const post = req.body;
+    if (!post.title || !post.content || !post.author) {
+        return res.status(400).json({success: false, message: "Missing title, content or author" });
+    }
+    try {
+        const updatedPost = await Post.findByIdAndUpdate(id, post, {new: true});
+        res.status(200).json({success: true, data: updatedPost });
+    } catch (error) {
+        res.status(500).json({success: false, message: error });
+    }
+})
+
 
 app.listen(5000, () => {
     connectDB();
